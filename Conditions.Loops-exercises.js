72d33e7e8172097825a735aa7895805a7ab6e1dc
april <apriltopolewski@gmail.com>
2020-11-04 21:59:34 -0500

    cleaned up JS Conditions Exercise 2

--- Conditions.Loops-exercises.js
+++ Conditions.Loops-exercises.js
@@ -17,8 +17,8 @@
 // Exercise 2: Identify longest of 3 names
     // Start with 3 variables (name1, name2, name3) which hold three names
         let name1 = "Anna"
-        let name2 = "Liam"
-        let name3 = "Rashmi"
+        let name2 = "Lia"
+        let name3 = "Bobb"
 
         console.log (name1.length)
         console.log (name2.length)
@@ -26,18 +26,18 @@
 
     // Output "<name> has the longest name". If there is a tie, both names should be listed in the sentence. If all names are the same length, output, "All three names, <name1>, <name2>, and <name3>, are the same length."
         // change names to nameX.length for statements
-        if (name1.length >(name2.length || name3.length)) {
-            console.log ("<name1> has the longest name.");
-        } else if (name2.length > (name1.length || name3.length)) {
-            console.log ("<name2> has the longest name.");
-        } else if (name3.length > (name1.length || name2.length)) {
-            console.log ("<name3> has the longest name.");
-        // } else if (name1 = name2 = name3) { split into two statements
-            console.log ("All three names, <name1>, <name2>, and <name3> are the same length.")
-        } else if ((name1 = name2) > name3) {
-            console.log ("<name1> and <name2> tie for the longest name.");
-        } else if ((name1 = name3) > name2) {
-            console.log ("<name1> and <name3> tie for the longest name.");
-        } else { // if (name2 === name3 > name1)
-            console.log ("<name2> and <name3> tie for the longest name.");
+        if ((name1.length) > ((name2.length) && (name3.length))) {
+            console.log (`${name1} has the longest name.`);
+        } else if (name2.length > (name1.length && name3.length)) {
+            console.log (`${name2} has the longest name.`);
+        } else if (name3.length > ((name1.length) && (name2.length))) {
+            console.log (`${name3} has the longest name.`);
+        } else if ((name1.length == name2.length) && (name1.length == name3.length)) {
+            console.log (`All three names, ${name1}, ${name2}, and ${name3} are the same length.`)
+        } else if ((name1.length = name2.length) > name3.length) {
+            console.log (`${name1} and ${name2} tie for the longest name.`);
+        } else if ((name1.length = name3.length) > name2.length) {
+            console.log (`${name1} and ${name3} tie for the longest name.`);
+        } else { if ((name2.length = name3.length) > name1.length)
+            console.log (`${name2} and ${name3} tie for the longest name.`);
         }
--- JS Functions Exercise 2/scripts.js
+++ JS Functions Exercise 2/scripts.js
@@ -14,6 +14,7 @@
 
     function moveBackward(distance) {
         // decrease position by distance
+        // moveForward(distance *-1) another way to do it with less code
         if (facingForward == true) {
             position = position - distance;
         } else {
@@ -37,5 +38,60 @@
     turnAround();
     moveForward(10);
     moveBackward(5);
+    printLocation();
+})();
+
+
+
+
+// HARD MODE
+(function() {
+    let facingForward = true;
+    let position = 0 N, 0 E;
+    let direction = North;
+
+    function moveForward(distance){
+        // how do I add direction here?
+        if (facingForward == true) {
+            position = position + distance;
+        } else {
+            position = position - distance;
+        }
+    }
+
+    function moveBackward(distance) {
+        // how do I add direction here?
+        if (facingForward == true) {
+            position = position - distance;
+        } else {
+            position = position + distance;
+        }
+        }   
+
+    // function turnAround () {
+    //     // switch value of facingForward
+    //     facingForward = !facingForward //will flip the facing to the opposite
+    // }
+
+    function turnLeft () {
+        // change direction 90* to the left
+    }
+
+    function turnRight () {
+        // change direction 90* to the right
+    }
+
+
+    function printLocation () {
+        console.log(position);
+    }
+
+    moveForward(5);
+    printLocation();
+    turnLeft();
+    turnLeft();
+    moveForward(7);
+    turnRight();
+    moveBackward(3);
     printLocation();
 })();
